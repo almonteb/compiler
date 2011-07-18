aa33ff0f45d53770f94340fefca2e99614620184
Brendan Almonte <almonteb@datawh.net>
2011-07-18 12:25:45 -0400

    moved terminals to a static class and created an ast junit test

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,8 @@
 import AST.*;
-import AST.Number;
+import static AST.Terminals.*;
 
 public class Main
-{
-	/* Static Functions */ 
-	public static Id id (String id) { return new Id (id); }
-	public static Number number (int n) { return new Number (n); }
-	public static Chars chars (String s) { return new Chars (s); }
-	public static Assign assign (Id var, Expression val) { return new Assign (var, val); }
-	public static Block block (Statement... statements) { return new Block (statements); }
-	public static Branch branch (Expression predicate, Statement ifBranch, Statement elseBranch) { return new Branch (predicate, ifBranch, elseBranch); }
-	public static Loop loop (Expression predicate, Statement body) { return new Loop (predicate, body); }
-	public static Plus plus (Expression left, Expression right) { return new Plus (left, right); }
-	public static Minus minus (Expression left, Expression right) { return new Minus (left, right); }
-	public static Times times (Expression left, Expression right) { return new Times (left, right); }
-	public static Divide divide (Expression left, Expression right) { return new Divide (left, right); }
-	
+{	
 	public static void main(String[] args)
 	{
 		/*
---
+++ test/ASTTests/Main.java
@@ -0,0 +1,27 @@
+package ASTTests;
+import AST.*;
+import static AST.Terminals.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Main
+{
+	@Test
+	public void testAst()
+	{
+		Node factorial = block(
+				assign(id("factorial"), number(1)),
+				assign(id("i"), number(5)),
+				assign(id("test"), chars("cookies")),
+				loop(id("i"),
+						block(assign(id("factorial"),
+								times(id("factorial"), id("i"))),
+								assign(id("i"), minus(id("i"), number(1))))));
+		StatementInterpreter runner = new StatementInterpreter();
+		factorial.accept(runner);
+		
+		assertEquals(runner.symbols.table.get("i"), 0);
+		assertEquals(runner.symbols.table.get("factorial"), 120);
+		assertEquals(runner.symbols.table.get("test"), "cookies");
+	}
+}
